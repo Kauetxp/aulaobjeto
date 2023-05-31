63b21ae7d18bb103f3a3572718ca0c33a3c72f6b
Kauetxp <123607569+Kauetxp@users.noreply.github.com>
2023-05-31 23:37:57 +0000

    Novo

--- exemplo7.py
+++ exemplo7.py
@@ -1,20 +1,19 @@
 #Mesma coisa do exemplo anterior porém de outra forma
-
 class Pessoa:
     def __init__(self,nome,idade):
         self.nome = nome
         self.idade = idade
-
     def escreve(self):
         print(self.nome, self.idade)
 
 class Estudante(Pessoa):
-    def __init__(self,nome,idade):
+    def __init__(self,nome,idade,ano):
         super().__init__(nome,idade)
+        self.graduacao = ano
+    def bemVindo(self):
+        print("Bem-vindo",self.nome,"para a classe", self.graduacao)
 
-        #Aqui eu estou adicionando um novo item a classe
-        self.graduacao = 2024
-x = Estudante("Jô",21)
-x.escreve()
-print(x.graduacao)
+x = Estudante("Jô",21,2024)
+
+x.bemVindo()
 
